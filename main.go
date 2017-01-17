c9463d53d3d5aa850225b9c6c454b7f24a2f223f
iamaya <imaya.kumar@yahoo.com>
2017-01-17 12:56:14 -0500

    Base checkin

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
 func main() {
+	mp := MessagePrinter{"foo", 5}
+	mp.PrintMessage()
 
+	emp := enhancedMessagePrinter{MessagePrinter{"bar", 10}}
+	emp.PrintMessage()
+	emp.EnhancedPrintMessage()
+}
+
+// MessagePrinter class has 2 members
+type MessagePrinter struct {
+	message string
+	number  int
+}
+
+// PrintMessage is a function that prints a message
+func (mp *MessagePrinter) PrintMessage() {
+	println(mp.message, mp.number)
+}
+
+// enhancedMessagePrinter is a enhancement struct
+type enhancedMessagePrinter struct {
+	MessagePrinter
+}
+
+func (emp *enhancedMessagePrinter) EnhancedPrintMessage() {
+	println("Enhanced -", emp.message, emp.number)
 }
